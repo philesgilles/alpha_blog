df64b77946b0d80c62cc4518e454e7c788e1a744
Philippe Gilles <philgdep@gmail.com>
2020-01-10 15:52:51 +0100

    wrap up and extract redundancy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only:[:edit,:update,:show,:destroy]
+    
     def index
         @articles = Article.all
     end
@@ -6,7 +8,6 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
     def edit
-        @article = Article.find(params[:id])
     end
     
     def create
@@ -21,7 +22,6 @@ class ArticlesController < ApplicationController
         end
     end
     def update
-        @article = Article.find(params[:id])
         if @article.update(article_params)
             flash[:notice] = "Article was successfully updated"
             redirect_to article_path(@article)    
@@ -33,17 +33,18 @@ class ArticlesController < ApplicationController
     
 
     def show
-        @article = Article.find(params[:id])
     end
     
     def destroy
-        @article = Article.find(params[:id])
         @article.destroy
         flash[:notice] = "Article was successfully deleted"
         redirect_to articles_path
         
     end
     private
+    def set_article
+        @article = Article.find(params[:id])
+    end
         def article_params
             params.require(:article).permit(:title,:description)
         end
