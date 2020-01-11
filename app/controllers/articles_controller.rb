44bf00b668d02d63a6ca84c7249e2a89d9bb9c9e
Philippe Gilles <philgdep@gmail.com>
2020-01-11 17:31:53 +0100

    awesome

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
         #render plain: params[:article].inspect
 
         @article = Article.new(article_params)
-        @article.user = User.find(10)
+        @article.user = User.first
         if @article.save
             flash[:success] = "Article was successfully created"
             redirect_to article_path(@article)    
