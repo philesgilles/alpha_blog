15f3de1ce06aad9b944e735cefe8fc7c9ddc950e
Philippe Gilles <philgdep@gmail.com>
2020-01-12 06:41:45 +0100

    add register and edit user

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   get 'about', to: 'pages#about'
-  get 'signup', to: 'users#new'
+  #all articles
   resources :articles
+  
+  #all users
+  get 'signup', to: 'users#new'
+  resources :users, except:[:new]
 end
