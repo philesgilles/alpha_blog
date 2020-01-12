dbbd7a2b5ab41444c5fa58e0d0c13faa6032d035
Philippe Gilles <philgdep@gmail.com>
2020-01-12 08:40:52 +0100

    add users listing and users to article

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,16 @@ class UsersController < ApplicationController
             render 'new'
         end
     end
+
+    def show
+        @user = User.find(params[:id])
+    end
+    
+    def index
+        @users = User.all
+        
+    end
+    
     
     private
     def user_params
