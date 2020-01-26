d2450475a43cbe07ba758ff6d163081897e68d0b
Philippe Gilles <philgdep@gmail.com>
2020-01-26 15:12:46 +0100

    add edit category possibility

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -23,6 +23,20 @@ class CategoriesController < ApplicationController
             render 'new'
         end
     end
+
+    def edit
+        @category = Category.find(params[:id])
+    end
+    def update
+        @category = Category.find(params[:id])
+        if @category.update(category_params)
+            flash[:success] = "Name updated successfully"
+            redirect_to category_path(@category)
+        else
+            flash[:danger] = "there was a problem"
+            render 'edit'
+        end
+    end
     
     private
     def category_params
