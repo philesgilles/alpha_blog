b57e390d816435980051fc8f9cda319399cfd588
Philippe Gilles <philgdep@gmail.com>
2020-01-26 07:32:37 +0100

    fix test and update navbar with categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,7 @@
 class CategoriesController < ApplicationController
+    before_action :require_admin, except: [:index,:show]
     def index
-        @categories = Category.all
+        @categories = Category.paginate(page: params[:page], per_page:5)
     end
 
     def new
@@ -23,8 +24,15 @@ class CategoriesController < ApplicationController
     end
     
     private
-        def category_params
-            params.require(:category).permit(:name)
-            
+    def category_params
+        params.require(:category).permit(:name)
+        
+    end
+    def require_admin
+        if !logged_in? || (logged_in? and !current_user.admin?)
+            flash[:danger] = "Only admin can perform this action"
+            redirect_to categories_path
         end
+        
+    end
 end
--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
     def setup
       @category = Category.create(name:"sports")
-      
+      @user = User.create(username: "john",email:"john@example.com",password:"password", admin:true)
     end
     
   
@@ -13,12 +13,21 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     end
 
     test "should get new" do
+      sign_in_as(@user,"password")
       get new_category_path
       assert_response :success
     end
 
     test "should get show" do
+      sign_in_as(@user,"password")
       get category_path(@category)
       assert_response :success
+    end
+
+    test "should redirect if not admin" do
+      assert_no_difference 'Category.count' do
+        post categories_path, params:{category:{name: "sports"}}
+      end
+      assert_redirected_to categories_path
     end
 end
